732f01ae44a1d87068f621629ac6de4680a3a942
Pedro Zich <zichpedro22@gmail.com>
2023-10-10 14:48:25 -0300

    feat: Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyles } from "./styles/global";
+import { Transactions } from "./pages/Transactions";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Hello World</h1>
       <GlobalStyles />
+
+      <Transactions />
     </ThemeProvider>
   );
 }
