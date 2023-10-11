db3abb213b9b3cc641d887e0bcad1927860e9067
Pedro Zich <zichpedro22@gmail.com>
2023-10-11 20:06:32 -0300

    feat: Transaction Type Buttons component

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -67,3 +67,34 @@ export const CloseButton = styled(Dialog.Close)`
   cursor: pointer;
   color: ${(props) => props.theme["gray-500"]};
 `;
+
+export const TransactionType = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 1rem;
+  margin: 0.5rem 0;
+`;
+
+interface TransactionTypeButtonProps {
+  variant: "income" | "outcome";
+}
+
+export const TransactionTypeButton = styled.button<TransactionTypeButtonProps>`
+  background: ${(props) => props.theme["gray-700"]};
+  color: ${(props) => props.theme["gray-300"]};
+  border: 0;
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 6px;
+  gap: 0.5rem;
+  cursor: pointer;
+
+  svg {
+    color: ${(props) =>
+      props.variant === "income"
+        ? props.theme["green-300"]
+        : props.theme["red-300"]};
+  }
+`;
